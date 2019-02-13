252c52df7784924f53375fda9bd57708f3f65e15
Grant.Zhu <1176808876@qq.com>
2019-02-13 23:54:47 +0800

    collect cmd logs

---
+++ cmdlogs.bat
@@ -0,0 +1,27 @@
+:: Windows command (cmd.exe) logging tool
+::
+:: How To Use
+:: 1. Create bat file
+::    [Filder Name]\cmdlogs.bat
+::
+:: 2. Set autorun setting
+::    reg add "HKEY_CURRENT_USER\Software\Microsoft\Command Processor" /v AutoRun /d [Filder Name]\cmdlogs.bat
+::    or
+::    reg add "HKEY_LOCAL_MACHINE\Software\Microsoft\Command Processor" /v AutoRun /d [Filder Name]\cmdlogs.bat
+::
+
+@echo off
+setlocal EnableDelayedExpansion
+set LogFolderName=%LOCALAPPDATA%\CMDLogs
+set LogFileName=%date:~3,4%_%date:~8,2%_%date:~11,2%.log
+
+IF NOT EXIST %LogFolderName% (md %LogFolderName%)
+IF NOT EXIST %LogFolderName%\%LogFileName% (echo timestamp,command>> %LogFolderName%\%LogFileName%)
+
+:getcmd
+set Command=
+set DateTime=%date:~0,3%_%date:~3,4%_%date:~8,2%_%date:~11,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+set /P Command=%cd%^>
+echo %DateTime%  !Command!>> %LogFolderName%\%LogFileName%
+call %Command%
+goto getcmd
